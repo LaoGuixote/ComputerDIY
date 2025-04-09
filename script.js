71915ab4278895b964519540466c2a2c4bbc43fb
LaoGuixote <c96062@gmail.com>
2025-04-09 16:25:41 +0800

    第八版-完成下拉式選單&線上配單介面初步設計&彈出式登入視窗

---
+++ script.js
@@ -0,0 +1,10 @@
+function openLogin(){
+    document.getElementById("loginModal").style.display = "block";
+}   // 顯示登入視窗
+
+function closeLogin(){
+    document.getElementById("loginModal").style.display = "none";
+}   //隱藏登入視窗
+
+
+
